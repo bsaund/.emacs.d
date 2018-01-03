71a5128922c52ca7434e117fcf39ecc9bcab0629
Brad Saund <brad.saund@gmail.com>
2018-01-03 18:36:42 -0500

    Added ycmd support

--- init.el
+++ init.el
@@ -113,87 +113,82 @@
 
 
 
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                                         ;YCMD
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; Snippets
-;; (use-package yasnippet
-;;   :ensure t
-;;   :diminish yas-minor-mode
-;;   :init (yas-global-mode t))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                                        ;YCMD
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Snippets
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :init (yas-global-mode t))
 
-;; ;; Autocomplete
-;; (use-package company
-;;   :defer 10
-;;   :diminish company-mode
-;;   :bind (:map company-active-map
-;;               ("M-j" . company-select-next)
-;;               ("M-k" . company-select-previous))
-;;   :preface
-;;   ;; enable yasnippet everywhere
-;;   (defvar company-mode/enable-yas t
-;;     "Enable yasnippet for all backends.")
-;;   (defun company-mode/backend-with-yas (backend)
-;;     (if (or 
-;;          (not company-mode/enable-yas) 
-;;          (and (listp backend) (member 'company-yasnippet backend)))
-;;         backend
-;;       (append (if (consp backend) backend (list backend))
-;;               '(:with company-yasnippet))))
+;; Autocomplete
+(use-package company
+  :defer 10
+  :diminish company-mode
+  :bind (:map company-active-map
+              ("M-j" . company-select-next)
+              ("M-k" . company-select-previous))
+  :preface
+  ;; enable yasnippet everywhere
+  (defvar company-mode/enable-yas t
+    "Enable yasnippet for all backends.")
+  (defun company-mode/backend-with-yas (backend)
+    (if (or 
+         (not company-mode/enable-yas) 
+         (and (listp backend) (member 'company-yasnippet backend)))
+        backend
+      (append (if (consp backend) backend (list backend))
+              '(:with company-yasnippet))))
 
-;;   :init (global-company-mode t)
-;;   :config
-;;   ;; no delay no autocomplete
-;;   (validate-setq
-;;    company-idle-delay 0
-;;    company-minimum-prefix-length 2
-;;    company-tooltip-limit 20)
+  :init (global-company-mode t)
+  :config
+  ;; no delay no autocomplete
+  (validate-setq
+   company-idle-delay 0
+   company-minimum-prefix-length 2
+   company-tooltip-limit 20)
 
-;;   (validate-setq company-backends 
-;;                  (mapcar #'company-mode/backend-with-yas company-backends)))
-
-
-
-;; ;; Code-comprehension server
-;; (use-package ycmd
-;;   :ensure t
-;;   :init (add-hook 'c++-mode-hook #'ycmd-mode)
-;;   :config
-;;   (set-variable 'ycmd-server-command '("python2" "/home/bradsaund/programs/ycmd/ycmd"))
-;;   (set-variable 'ycmd-global-config (expand-file-name "/home/bradsaund/programs/ycmd/cpp/ycm/.ycm_extra_conf.py"))
-
-;;   (set-variable 'ycmd-extra-conf-whitelist '("~/catkin_ws/*"))
-
-;;   (use-package company-ycmd
-;;     :ensure t
-;;     :init (company-ycmd-setup)
-;;     :config (add-to-list 'company-backends (company-mode/backend-with-yas 'company-ycmd))))
+  (validate-setq company-backends 
+                 (mapcar #'company-mode/backend-with-yas company-backends)))
 
 
-;; ;; On-the-fly syntax checking
-;; (use-package flycheck
-;;   :ensure t
-;;   :diminish flycheck-mode
-;;   :init (global-flycheck-mode t))
 
-;; (use-package flycheck-ycmd
-;;   :commands (flycheck-ycmd-setup)
-;;   :init (add-hook 'ycmd-mode-hook 'flycheck-ycmd-setup))
+;; Code-comprehension server
+(use-package ycmd
+  :ensure t
+  :init (add-hook 'c++-mode-hook #'ycmd-mode)
+  :config
+  (set-variable 'ycmd-server-command '("python2" "/home/bradsaund/programs/ycmd/ycmd"))
+  (set-variable 'ycmd-global-config (expand-file-name "/home/bradsaund/programs/ycmd/cpp/ycm/.ycm_extra_conf.py"))
 
-;; ;; Show argument list in echo area
-;; (use-package eldoc
-;;   :diminish eldoc-mode
-;;   :init (add-hook 'ycmd-mode-hook 'ycmd-eldoc-setup))
+  (set-variable 'ycmd-extra-conf-whitelist '("~/catkin_ws/*"))
 
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  )
+  (use-package company-ycmd
+    :ensure t
+    :init (company-ycmd-setup)
+    :config (add-to-list 'company-backends (company-mode/backend-with-yas 'company-ycmd))))
+
+
+;; On-the-fly syntax checking
+(use-package flycheck
+  :ensure t
+  :diminish flycheck-mode
+  :init (global-flycheck-mode t))
+
+(use-package flycheck-ycmd
+  :commands (flycheck-ycmd-setup)
+  :init (add-hook 'ycmd-mode-hook 'flycheck-ycmd-setup))
+
+;; Show argument list in echo area
+(use-package eldoc
+  :diminish eldoc-mode
+  :init (add-hook 'ycmd-mode-hook 'ycmd-eldoc-setup))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
